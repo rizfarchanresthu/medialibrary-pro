b1423ffc55d804e6c25e7514b7304a6fdbaaf0f8
Resthu Rizfarchan Adisurya <resthu.adisurya@brids.co.id>
2024-03-26 15:02:10 +0700

    test upload all

---
+++ resources/views/components/media-library-attachment.blade.php
@@ -0,0 +1,17 @@
+@include('media-library::components.partials.media-library-once')
+<livewire:media-library
+    :key="'media-library-component' . $name"
+    :media="$media"
+    :name="$name"
+    :rules="$rules"
+    :accept="$accept"
+    :multiple="$multiple"
+    :maxItems="$determineMaxItems()"
+    :view="$componentView ?? null"
+    :sortable="false"
+    :listView="$determineListViewName()"
+    :itemView="$determineItemViewName()"
+    :propertiesView="$propertiesView ?? null"
+    :fieldsView="$determineFieldsViewName()"
+    :editableName="$editableName"
+/>
---
+++ resources/views/livewire/partials/thumb.blade.php
@@ -0,0 +1,20 @@
+<div class="media-library-thumb" dusk="thumb">
+    @if($mediaItem->previewUrl)
+        <img
+            class="media-library-thumb-img" src="{{ $mediaItem->previewUrl }}"
+            alt="{{ $mediaItem->fileName }}">
+    @else
+        <span class="media-library-thumb-extension">
+            <span
+                class="media-library-thumb-extension-truncate">{{ $mediaItem->extension }}</span>
+        </span>
+    @endif
+
+    <livewire:media-library-uploader
+        :key="'thumb-uploader' . $mediaItem->uuid"
+        :name="$this->name"
+        :rules="$rules"
+        :accept="$accept ?? []"
+        :uuid="$mediaItem->uuid"
+    />
+</div>
